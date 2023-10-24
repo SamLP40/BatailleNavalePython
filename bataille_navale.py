1ab3dfa14f61e71e92a955f9360a6b114e1ae517
SamSam <sam40230@gmail.com>
2023-10-24 16:02:35 +0200

    New warship battle

---
+++ bataille_navale.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+#  -*- coding: utf-8 -*-
+
+# la grille de jeu virtuelle est composée de 10 x 10 cases
+# une case est identifiée par ses coordonnées, un tuple (no_ligne, no_colonne)
+# un no_ligne ou no_colonne est compris dans le programme entre 1 et 10,
+# mais pour le joueur une colonne sera identifiée par une lettre (de 'A' à 'J')
+
+GRID_SIZE = 10
+
+# détermination de la liste des lettres utilisées pour identifier les colonnes :
+LETTERS = "ABCDEFGHIJ"
+
+# chaque navire est constitué d'un dictionnaire dont les clés sont les
+# coordonnées de chaque case le composant, et les valeurs correspondantes
+# l'état de la partie du navire correspondant à la case
+# (True : intact ; False : touché)
+
+# les navires suivants sont disposés de façon fixe dans la grille :
+#      +---+---+---+---+---+---+---+---+---+---+
+#      | A | B | C | D | E | F | G | H | I | J |
+#      +---+---+---+---+---+---+---+---+---+---+
+#      | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10|
+# +----+---+---+---+---+---+---+---+---+---+---+
+# |  1 |   |   |   |   |   |   |   |   |   |   |
+# +----+---+---+---+---+---+---+---+---+---+---+
+# |  2 |   | o | o | o | o | o |   |   |   |   |
+# +----+---+---+---+---+---+---+---+---+---+---+
+# |  3 |   |   |   |   |   |   |   |   |   |   |
+# +----+---+---+---+---+---+---+---+---+---+---+
+# |  4 | o |   |   |   |   |   |   |   |   |   |
+# +----+---+---+---+---+---+---+---+---+---+---+
+# |  5 | o |   | o |   |   |   |   | o | o | o |
+# +----+---+---+---+---+---+---+---+---+---+---+
+# |  6 | o |   | o |   |   |   |   |   |   |   |
+# +----+---+---+---+---+---+---+---+---+---+---+
+# |  7 | o |   | o |   |   |   |   |   |   |   |
+# +----+---+---+---+---+---+---+---+---+---+---+
+# |  8 |   |   |   |   |   |   |   |   |   |   |
+# +----+---+---+---+---+---+---+---+---+---+---+
+# |  9 |   |   |   |   | o | o |   |   |   |   |
+# +----+---+---+---+---+---+---+---+---+---+---+
+# | 10 |   |   |   |   |   |   |   |   |   |   |
+# +----+---+---+---+---+---+---+---+---+---+---+
+aircraft_carrier = {}  # porte_avion en B2
+cruiser = {}  # croiseur en A4
+destroyer = {}  # contre_torpilleur en C5
+submarine = {}  # sous_marin en H5
+torpedo_boat = {}  # torpilleur en E9
+ships_list = [aircraft_carrier, cruiser, destroyer, submarine, torpedo_boat]
+
+# l'embryon de notre jeu consiste à demander à l'infini au joueur les coordonnées
+# d'un tir, puis à indiquer si ce tir touche un navire (en mémorisant les conséquences
+# de ce tir, indiquant si le navire est coulé à la suite de plusieurs tirs convergents,
+# et si la partie est finie lorsque le dernier navire est coulé)
+
+#Génération du plateau de jeu
+for _ in range(GRID_SIZE):
+    board = []
+    row = ["0"] * GRID_SIZE
+    board.append(row)
+    for row in board:
+        print(" ".join(row))
+
+while True:
+    print("Indiquez les coordonnées de votre tir (ex: A5, H9...) :")
+
+    break
